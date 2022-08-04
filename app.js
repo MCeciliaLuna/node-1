12f5be565ac8a84f401fa42d6af99567cdc74e28
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-08-04 19:30:43 -0300

    nodemon + primeras prácticas de postman y express

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express') // llamar express
-const app = express() //guardar en una constante toda la funcionalidad importada arriba | esta constante puedo usar para generar rutas
+const app = express() //guardar en una constante toda la funcionalidad importada arriba | esta constante puedo usar para generar rutas o acciones
 const port = 8000 // puerto donde vamos a alojar nuestro servidor del backend
 
 app.get('/', (req, res) => { //app . (este punto puede llamar a GET, PATCH, PUT, POST, DELETE)
-  res.send('Hola mundo!')
+  res.send('Mensaje enviado')
+})
+
+app.delete('/delete', (req, res) => {
+  res.status(404).json({ //mandar el status
+    message: "Eliminación exitosa", //generalmente siempre mandaremos objetos
+    id: 404
+  })
+})
+
+app.post('/post', (req, res) => {
+  console.log(req)
+  res.json(body)
 })
 
 app.listen(port, () => {
-  console.log(`Servidor corriendo en puerto ${8000}`)
+  console.log(`Servidor corriendo en puerto ${8000}`) //ver el resultado del puerto
 })
